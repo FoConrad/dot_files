84486ef95a1c31029b06c822b8824faf16d044a9
Conrad Christensen <conradbchristensen@gmail.com>
2020-07-27 22:25:52 -0400

    organize my oh-my-zsh mods

---
+++ oh-my-zsh-mods/make_links.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+OH_MY_ZSH_REL_PATH="../oh-my-zsh"
+
+check_n_link() {
+    if [[ "$#" -ne 2 ]]; then
+        >&2 echo "Please pass absolute path of real_file then the link_file"
+        exit 1
+    fi
+
+    local real_file="$1"
+    local link_file="$2"
+
+    if [[ -e "${link_file}" ]]; then
+        >&2 echo "A file already exists where you want the link"
+        ls -l "${link_file}"
+    fi
+
+    ln -s "${real_file}" "${link_file}"
+}
+
+# My theme
+check_n_link "$(pwd)/conrad.zsh-theme" "${OH_MY_ZSH_REL_PATH}/themes/conrad.zsh-theme"
+
+# directories lib
+check_n_link "$(pwd)/directories.lib.zsh " "${OH_MY_ZSH_REL_PATH}/lib/directories.zsh"
+
+# git plugin
+check_n_link "$(pwd)/git.plugin.zsh" "${OH_MY_ZSH_REL_PATH}/plugins/git/git.plugin.zsh"
+
+# tmux plugin
+check_n_link "$(pwd)/tmux.plugin.zsh" "${OH_MY_ZSH_REL_PATH}/plugins/tmux/tmux.plugin.zsh"
+
+# vi-mode plugin
+check_n_link "$(pwd)/vi-mode.plugin.zsh" "${OH_MY_ZSH_REL_PATH}/plugins/vi-mode.plugins.zsh"
